ff37d6e21883103b8fb1d9ff0d3cfb49b408e57a
magarini <56253774+magarini@users.noreply.github.com>
2020-02-29 16:50:09 +0200

    Update bed.js

--- bed.js
+++ bed.js
@@ -3,9 +3,7 @@ let teapot;
 let laptop;
 let bed;
 let stars;
-let love;
-let time;
-let h,m,s;
+let j=0;
 
 function preload() {
     // Load model with normalise parameter set to true
@@ -16,40 +14,23 @@ function preload() {
 
     img = loadImage('fabric.jpg');
    
-   
+    soundFormats('mp3', 'ogg');
+    mySound = loadSound('bedroom site.mp3');
 
   }
 
 function setup(){
     let canvas= createCanvas(windowWidth,windowHeight,WEBGL);
     textureMode(IMAGE);
-    // textureWrap(CLAMP);
-    
-    love = createGraphics(300, 300);
-    //love.background(255, 100);
-    love.background(random(10,50));
-    love.fill(255);
-    love.textAlign(CENTER);
-    love.textSize(20);
-    // love.text('5AM', 200, 100);
+  
     frameRate(10);
-   //ortho(-width, width, height, -height/2, 0.1, 100);
   
   }
 
 
   function draw(){
-    // s=second();
-    // m=minute();
-    // h=hour();
-    clear();
-  
-  //   time=(h+':'+m+':'+s);
-  //   console.log(time);
-  //  love.text(time, 200, 100);
 
-    //background(0);
-    
+    clear();
     background(0);
     texture(stars);
     push();
@@ -61,49 +42,42 @@ function setup(){
 
         pointLight(255,mouseX,mouseY);
        fill(250,235,215);
-// box(2000,2000,3000);
     angleMode(DEGREES);
-    //ambientLight(255);
-    
     pointLight(255,mouseX,mouseY);
-  // rotateZ(frameCount * 0.1);
-  // rotateX(200);
-
-
-  
 
 push();
 pointLight(255,mouseX,mouseY);
-// rotateX(frameCount * 0.1);
 rotateZ(90);
 rotateX(90);
-// rotateY(90);
 noStroke();
 specularMaterial(50);
-//  texture(vid);
-//shininess(10);
-// scale(3);
-// (k,z,x)
-// texture(love);
+
 
 translate(200,-1000,50);
 scale(3);
-// fill(80);
-// blendMode(MULTIPLY);
+
 
 model(laptop);
 pop();
 
-// fill(200);
 noStroke();
 texture(img);
 
 scale(12);
 rotateZ(180);
-// rotateX(90);
 translate(0,-50,-80);
 
 model(bed);
 
 
   }
+
+  function mousePressed(){
+    j=j+1;
+    if (j<2){
+    mySound.setVolume(0.4);
+    mySound.loop();
+  }
+  }
+
+  
